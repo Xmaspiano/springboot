5ae59a058287c1d18d7f844baa0714c5cf0647a7
Albert ZheYang <xmaspiano@Hotmail.com>
2018-03-01 15:00:14 +0800

    update by xmas from mac this is unfinish by the update maven 工程化 文件结构变更 补充遗失文件 xmaspiano@hotmail.com

---
+++ system/src/main/java/com/springboot/system/entity/secondDsE/Hrmdepartment.java
@@ -0,0 +1,37 @@
+package com.springboot.system.entity.secondDsE;
+
+
+import com.springboot.common.entity.BaseIdEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**  
+ *    
+ *   
+ * @author XmasPiano  
+ * @date 2018/3/1 上午10:31
+ * @param   
+ * @return   
+ */  
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+public class Hrmdepartment extends BaseIdEntity<Hrmdepartment> {
+    private String departmentname;
+    private Long subcompanyid1;
+    private Long supdepid;
+    private String canceled;
+    private String tlevel;
+
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @JoinColumn(name = "supdepid", updatable = false)
+    private List<Hrmdepartment> hrmDepartmentList = new ArrayList<Hrmdepartment>();
+}
